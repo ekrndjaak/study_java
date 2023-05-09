19db00cd5e5d9ee463e400fe6f675921b9e02a1a
ekrndjaak <dlagksrb0430@naver.com>
2023-05-09 12:25:50 +0900

    0509 practice

---
+++ src/Practice/CountMoney.java
@@ -0,0 +1,15 @@
+/* 화폐개수 세기
+10만원 짜리 수표가 들어가는 자판기를 만들려고한다. 자판기에 수표를 입력하면 거스름돈을 계산하여
+정확하게 필요한 화폐들의 개수를 계산하여야 한다. 이를 계산하는 프로그램을 작성하시오
+ */
+package Practice;
+import java.util.Scanner;
+public class CountMoney {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("거스름돈을 입력하세요 ");
+        int charge = Integer.parseInt(sc.nextLine());
+
+        System.out.println("5만원권" + (charge / 50000) + "개");
+    }
+}
---
+++ src/Practice/KmChange.java
@@ -0,0 +1,20 @@
+// meter를 입력받아 km 와 m 로 출력하는 프로그램을 작성하시오.
+package Practice;
+import java.util.Scanner;
+import java.awt.datatransfer.StringSelection;
+
+public class KmChange {
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("거리를 입력하세요 :");
+        int i = Integer.parseInt(sc.nextLine());
+        int k = 0;
+        if (i < 1000) {
+            System.out.println(i + "m");
+        } else {
+            k = i / 1000;
+            System.out.println(k + "km");
+        }
+    }
+}
