850466bb92cdabf3296a6eb7afe5d963f1280f61
ekrndjaak <dlagksrb0430@naver.com>
2023-05-11 03:00:15 +0900

    0511 program

--- src/TEST/Test1.java
+++ src/TEST/Test1.java
@@ -6,10 +6,5 @@ public class Test1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Connect User Name : ");
-
-        int UserName = Integer.parseInt(sc.nextLine());
-
-        System.out.printf(" %s 님! 아이디를 입력해주세요", UserName);
-
     }
 }
